11b014920dd7db5e192fcf85808bdb14863e1214
sun-moon-star-star <75414051+sun-moon-star-star@users.noreply.github.com>
2021-03-22 19:27:04 +0800

    feat(heap.go): init

---
+++ heap.go
@@ -0,0 +1,26 @@
+package heap
+
+type Heap struct {
+	data []int
+}
+
+func (heap *Heap) Less(i, j int) bool {
+	return (heap.data)[i] < (heap.data)[j]
+}
+
+func (heap *Heap) Swap(i, j int) {
+	(heap.data)[i], (heap.data)[j] = (heap.data)[j], (heap.data)[i]
+}
+
+func (heap *Heap) Len() int {
+	return len(heap.data)
+}
+
+func (heap *Heap) Pop() (v interface{}) {
+	heap.data, v = (heap.data)[:heap.Len()-1], (heap.data)[heap.Len()-1]
+	return
+}
+
+func (heap *Heap) Push(v interface{}) {
+	heap.data = append(heap.data, v.(int))
+}
